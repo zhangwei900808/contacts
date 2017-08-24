7208991eb430fc9593ed1199da6590609d40f0fb
张威 <zw900808@gmail.com>
2017-08-24 15:53:26 +0800

    添加登录按钮

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,9 +3,10 @@
         <b-navbar toggleable="md" type="dark" variant="primary" fixed="top">
             <b-nav-toggle target="nav_collapse"></b-nav-toggle>
                 <b-navbar-brand to="/">Team247联系人管理</b-navbar-brand>
-                <b-collapse is-nav id="nav_collapse" v-if="isLogin">
+                <b-collapse is-nav id="nav_collapse">
                     <b-nav is-nav-bar class="ml-auto">
-                        <b-nav-item-dropdown right>
+                        <b-nav-item to="/login"  v-if="!isLogin">登录</b-nav-item>
+                        <b-nav-item-dropdown right  v-if="isLogin">
                             <template slot="button-content">
                             <em>{{userInfo.name}}</em>
                             </template>
