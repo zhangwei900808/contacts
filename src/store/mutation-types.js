bab67b5f631c9727141d9dc14f5ddadc4aee0ade
张威 <zw900808@gmail.com>
2017-08-26 15:56:10 +0800

    修改store文件mutation类型

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,7 +1,9 @@
 // contactList
 export const GET_ALL_CONTACT_LIST = 'contactList/GET_ALL_CONTACT_LIST'
 export const ADD_CONTACT = 'contactList/ADD_CONTACT'
-
+export const GET_CONTACT_BY_ID = 'contactList/GET_CONTACT_BY_ID'
+export const UPDATE_CONTACT_BY_ID = 'contactList/UPDATE_CONTACT_BY_ID'
+export const DELETE_CONTACT_BY_ID = 'contactList/DELETE_CONTACT_BY_ID'
 
 // login
 export const SET_LOGIN_INFO ='login/SET_LOGIN_INFO'
