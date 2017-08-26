d3b9d0ed8eccbedb7210608dd66a7bedbda8ca06
张威 <zw900808@gmail.com>
2017-08-26 15:55:05 +0800

    修改联系人操作数据的store文件

--- src/store/modules/ContactList/index.js
+++ src/store/modules/ContactList/index.js
@@ -1,29 +1,55 @@
 import * as types from '@/store/mutation-types'
 import axios from 'axios'
-import qs from 'qs'
 
 export default {
     state:{
       contacts:[]
     },
     mutations:{
+        // 根据联系人id删除联系人信息
+        [types.DELETE_CONTACT_BY_ID](state,obj){
+            axios.delete(`http://localhost:8089/contacts/${obj.id}`)
+                .then((res)=>{
+                    obj.success()
+                }).catch((err)=>{
+                    console.log(`msg:${err}`)
+                })
+        },
+        // 根据联系人id更新联系人信息
+        [types.UPDATE_CONTACT_BY_ID](state,obj){
+            axios.put(`http://localhost:8089/contacts/${obj.contact.id}`,obj.contact)
+            .then((res)=>{                
+                obj.success();
+            })
+            .catch((err)=>{
+                console.log(`msg:${err}`)
+            })
+        },
+        // 根据联系人id获取联系人信息
+        [types.GET_CONTACT_BY_ID](state,obj){
+            axios.get(`http://localhost:8089/contacts/${obj.id}`)
+            .then((res)=>{                
+                console.log('contact by id:')
+                console.log(res.data);
+                obj.success(res.data);
+            })
+            .catch((err)=>{
+                console.log(`msg:${err}`)
+            })
+        },
         // 获取所有联系人信息
-      [types.GET_ALL_CONTACT_LIST](state){
+        [types.GET_ALL_CONTACT_LIST](state){
             axios.get('http://localhost:8089/contacts')
-                .then((res)=>{                
-                    res.data.map((contact,index)=>{
-                        contact.index = index+1;
-                        contact.fullName = contact.first_name+contact.last_name
-                    })
+                .then((res)=>{  
                     console.log(res.data);
                     state.contacts = res.data;
                 })
                 .catch((err)=>{
                     console.log(`msg:${err}`)
                 })
-      },
-      // 添加联系人
-      [types.ADD_CONTACT](state,obj){
+        },
+        // 添加联系人
+        [types.ADD_CONTACT](state,obj){
             axios.post('http://localhost:8089/contacts',obj.contact)
                 .then((res)=>{                
                     obj.success();
@@ -31,14 +57,23 @@ export default {
                 .catch((err)=>{
                     console.log(`msg:${err}`)
                 })
-      }
+        }
     },
     actions:{
+        getContactById(context,obj){
+            context.commit(types.GET_CONTACT_BY_ID,obj);
+        },
         getContacts(context){
             context.commit(types.GET_ALL_CONTACT_LIST);
         },
         addContact(context,obj){
             context.commit(types.ADD_CONTACT,obj);
+        },
+        updateContactById(context,obj){
+            context.commit(types.UPDATE_CONTACT_BY_ID,obj);
+        },
+        deleteContactById(context,obj){
+            context.commit(types.DELETE_CONTACT_BY_ID,obj);
         }
     }
   };
