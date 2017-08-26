d002d05d35e9f09bb0ef20e9ea237620c4bdf7aa
张威 <zw900808@gmail.com>
2017-08-26 15:48:40 +0800

    修改关于页面

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,16 +1,12 @@
 <template>
-    <section class="not-found-container">
-        <h1>关于</h1>
-        <p>Team247联系人管理系统</p>
+    <section class="about-container">
+        <h3>Team247联系人管理</h3>
+        <p>基于vue+vue-router+vuex+bootstrap-vue+axios</p>
+        <p>作者：张威 2017年8月</p>
     </section>
 </template>
-<script>
-    export default{
-
-    }
-</script>
 <style lang="less">
-    .not-found-container{
+    .about-container{
         padding-top:50px;
         text-align:center;
     }
